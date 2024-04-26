4f1b3662ed2b0501823eb4eaf1b6b818f2430b3e
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2024-04-26 14:27:21 +0300

    Add homework2

--- homework2/task1.py
+++ homework2/task1.py
@@ -1,6 +1,27 @@
+from functools import reduce
+
 import pandas as pd
 
-df=pd.read_csv('../../AB_NYC_2019.csv')
+
+def mapper(ind):
+    n = 1
+    score = df['price'][ind]
+    return score, score ** 2, n
+
+
+def reducer(score_data1, score_data2):
+    s1, s12, n1 = score_data1
+    s2, s22, n2 = score_data2
+    return s1 + s2, s12 + s22, n1 + n2
+
+
+df = pd.read_csv('../../AB_NYC_2019.csv')
 print(f'Mean value: {df['price'].mean()}.')
 print(f'Variance: {df['price'].var()}.')
 
+mx, mx2, num = reduce(reducer, map(mapper, list(df.index)))
+mean = mx / num
+disp = (mx2 / num - (mx / num) ** 2)
+
+print(f'Mean value: {mean}.')
+print(f'Variance: {disp}.')
